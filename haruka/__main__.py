a5907dd79eb6e07eae8f1a2435d225a16d2688be
UserBot69 <50572025+UserBot69@users.noreply.github.com>
2019-10-03 15:11:27 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -25,7 +25,7 @@ PM_START = """Hello {}, my name is {}!
 You are too concerned with what was and what will be. There is a saying: 
 "Yesterday is history, tomorrow is a mystery, but today is a gift. That is why it is called the present."
 
-I'm world manager bot maintained by [this awesome person](https://t.me/sunidhijain)!
+I'm a world manager bot maintained by [this cute one](https://t.me/sunidhijain)!
 
 Click Help button to find out more about how to use me to my full potential.
 
